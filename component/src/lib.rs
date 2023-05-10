9e60a4070460e8884bb00600729b1d8f7b42a908
Cameron <cameron.studdstreet@gmail.com>
2023-05-10 16:23:38 +0100

    the actual demo

--- component/src/lib.rs
+++ component/src/lib.rs
@@ -1,4 +1,4 @@
-use serde_json::{from_str, json, to_string_pretty};
+use serde_json::{json, to_string};
 
 wit_bindgen::generate!("hermes");
 
@@ -9,44 +9,27 @@ impl Hermes for Host {
     fn on_new_cardano(block: CardanoBlock) {
         let prime = primes::is_prime(block.slot);
 
-        let hash_of_first_bytes = match block.raw_bytes.len() {
-            0..=9 => vec![], // empty vec if the block is too small
-            _ => blake_hash(&block.raw_bytes[0..10]),
-        };
+        let hash = hex::encode(&block.hash);
 
         let payload = json!({
+            "hash": hash,
             "slot": block.slot,
-            "hash": block.hash,
-            "hash_of_first_bytes": hash_of_first_bytes,
-            "is_prime": prime,
+            "slot_is_prime": prime,
         });
 
-        println!("hello");
-        let json_string = to_string_pretty(&payload).unwrap();
+        let json_string = to_string(&payload).unwrap();
 
         publish(&json_string);
     }
 
     fn on_new_ethereum(rpc: wit_bindgen::rt::string::String) {
-        #[derive(serde::Deserialize)]
-        struct JsonRpc<'a> {
-            id: u64,
-            jsonrpc: &'a str,
-        }
+        let len = rpc.as_bytes().len();
+        let head = rpc.chars().take(20).collect::<String>();
 
-        let JsonRpc { id, jsonrpc } = from_str(&rpc).unwrap();
-
-        let prime = primes::is_prime(id);
-
-        let payload = json!({
-            "id": id,
-            "version": jsonrpc,
-            "is_prime": prime,
-        });
-
-        let msg = format!("look at this cool new eth RPC event: {payload}");
+        let msg = format!(
+            "look at this cool new eth RPC event - it has {len} bytes and starts with {head}"
+        );
 
         tweet(&msg);
     }
 }
-
